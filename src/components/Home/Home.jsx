41d91576f0649a5b649bc65bc275e6fc2831dafa
adams322 <joypender@outlook.com>
2023-11-28 09:13:58 -0800

    first commit
    this is my portfolio project

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,40 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particle";
+import Home2 from "./Home2";
+import Type from "./Type";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
+            <Col md={5} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img">
+                  👋🏻
+                </span>
+              </h1>
+              <div></div>
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> Oliver Grant</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            
+          </Row>
+        </Container>
+      </Container>
+      <Home2 />
+    </section>
+  );
+}
+
+export default Home;
---
+++ src/components/Home/Home2.jsx
@@ -0,0 +1,81 @@
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+} from "react-icons/ai";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple">  Javascript and React. </b>
+              </i>
+              <br />
+              <br />
+              My Ultimate purpose is to &nbsp;
+              <i>
+                <b className="purple">Satisfy all of you </b> give enjoy them.{" "}
+                <b className="purple">
+                  Perfectly
+                </b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing products
+              with <b className="purple">My own skills</b> and
+              <i>
+                <b className="purple">
+                  {" "}
+                  working with good feelings
+                </b>
+              </i>
+              &nbsp; by using
+              <i>
+                <b className="purple"> React.js and Next.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/adams322"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
